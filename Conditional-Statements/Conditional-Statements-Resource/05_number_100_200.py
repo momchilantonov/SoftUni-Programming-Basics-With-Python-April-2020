4116be628e4b8d7218af5140b371a5d15d738337
momchilantonov <eng.antonov@gmail.com>
2021-02-02 17:02:54 +0200

    Delete files

--- Conditional-Statements/Conditional-Statements-Resource/05_number_100_200.py
+++
@@ -1,8 +0,0 @@
-num = int(input())
-
-if num < 100:
-    print('Less than 100')
-elif num <= 200:
-    print('Between 100 and 200')
-elif num > 200:
-    print('Greater than 200')
--- Conditional-Statements/Conditional-Statements-Resource/06_password_guess.py
+++
@@ -1,7 +0,0 @@
-enter_password = input()
-password = 's3cr3t!P@ssw0rd'
-
-if enter_password == password:
-    print('Welcome')
-else:
-    print('Wrong password!')
